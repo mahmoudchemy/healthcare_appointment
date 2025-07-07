f0c1f0f2e0d8f67f0f00d90a8014ff2c01af5b6a
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-07-07 13:39:47 +0100

    'implement the changes made to the app'

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (appointment:CreateAppointmentParams) => {
 try {
@@ -35,3 +36,34 @@ export const getAppointment = async (appointmentId:string) => {
     }
 }
 
+export const getRecentAppointmentsList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            '6866950d002347c76191',
+            '68669583002153514fb7',
+            [Query.orderDesc('$createdAt')]
+        );
+
+        const initialCounts = {
+            scheduledCount:0,
+            pendingCount:0,
+            cancelledCount:0,
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+            if(appointment.status === 'scheduled') {
+                acc.scheduledCount += 1;
+            }else if (appointment.status === 'pending'){
+                acc.pendingCount += 1;
+            }else if (appointment.status === 'cancelled'){
+                acc.cancelledCount +=1;
+            }
+
+            return acc;
+
+        }, initialCounts);
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
